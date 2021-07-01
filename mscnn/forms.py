9cdbfc1458654f114069fed72151a69d2fc173f5
Shubham Darak <47272305+shubbham37@users.noreply.github.com>
2021-07-01 17:20:52 +0530

    ccform and mscnn apps of project

---
+++ mscnn/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import *
+
+#class predictionForm(forms.ModelForm):
+ #   class Meta:
+  #      model = countPrediction
+   #     fields = ['CrowdLimit','InputImage']
